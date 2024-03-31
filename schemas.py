69fc5af060b6c39f3cdfeeca619c90e279db0e85
Tetyana <kucherenkotatyana97@gmail.com>
2024-03-31 21:28:14 +0300

    Many-to-many relationships added.

--- schemas.py
+++ schemas.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields
 
 class PlainItemSchema(Schema):
     """Thanks for the schema we don't have to check incoming data in our View"""
-    id = fields.Str(dump_only=True)    # used only for sending back to the client
+    id = fields.Int(dump_only=True)    # used only for sending back to the client
     name = fields.Str(required=True)    # is required for both serialization and deserialization
     price = fields.Float(required=True)
 
@@ -13,9 +13,15 @@ class PlainStoreSchema(Schema):
     name = fields.Str()
 
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 
 class ItemUpdateSchema(Schema):    # both are optional
@@ -26,3 +32,17 @@ class ItemUpdateSchema(Schema):    # both are optional
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
+
